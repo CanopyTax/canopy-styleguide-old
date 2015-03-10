6a0eb01c9a213373584083cd9581ce60a37bb0a7
Bret Little <bret.little@gmail.com>
2015-03-10 15:58:49 -0600

    Update style guide presentation

--- src/app.jsx
+++ src/app.jsx
@@ -6,12 +6,12 @@ var components = require('./components.js');
 var Sidebar = React.createClass({
   render: function () {
 	    return (
-				<div className="bs-flexible-sidenav +tall-top">
-					<div className="bs-flexible-sidenav__menu">
+				<div className="bss-flexible-sidenav">
+					<div className="bss-flexible-sidenav__menu">
 					{components.map(function(component) {
-						return <Link className="bs-flexible-sidenav__menu__item" to="component" params={{title: component.title}}>
-							<i className="fa fa-home bs-flexible-sidenav__menu__item__icon"></i>
-							<span className="bs-flexible-sidenav__menu__item__title">{component.title}</span>
+						return <Link className="bss-flexible-sidenav__menu__item" activeClassName="+active" to="component" params={{title: component.title}}>
+							<i className="fa fa-home bss-flexible-sidenav__menu__item__icon"></i>
+							<span className="bss-flexible-sidenav__menu__item__title">{component.title}</span>
 						</Link>;
 					})}
 					</div>
@@ -27,19 +27,19 @@ var App = React.createClass({
 	    var title = this.getParams().title;
 	    return (
 				<div>
-					<div className="bs-topNav">
-						<div className="bs-topnav__bar"></div>
-						<div className="bs-topnav__content">
-							<span className="bs-topnav__content__brand" href>
+					<div className="bss-topNav">
+						<div className="bss-topnav__bar"></div>
+						<div className="bss-topnav__content">
+							<span className="bss-topnav__content__brand" href>
 								<img src="logo.png" alt="Company Logo"/>
 							</span>
-							<ul className="bs-topnav__content__menu padding-left-28">
+							<ul className="bss-topnav__content__menu padding-left-28">
 								<li><a href="#">Beanstalk Style Guide</a></li>
 							</ul>
 						</div>
 					</div>
 					<Sidebar/>
-					<div style={{'margin-left': '240px', width: 'calc(100% - 260px)'}}>
+					<div className="bss-nav-content">
 						<RouteHandler/>
 					</div>
 				</div>
@@ -65,7 +65,7 @@ var Component = React.createClass({
 var Index = React.createClass({
   render: function () {
 	    return (
-				<div style={{'margin-left': '240px', width: 'calc(100% - 260px)'}}>
+				<div className="bss-nav-content">
 					<p>Our Styleguide</p>
 				</div>
 			);
