7555ce4000c8d50c964794b985dfda6bbec44ac4
Bret Little <bret.little@gmail.com>
2015-03-11 16:11:03 -0600

    Change the class names for text colors to not include the word text

--- src/components.js
+++ src/components.js
@@ -5,7 +5,9 @@ module.exports = [
 	{ title: "Buttons",			html: require('./components/forms/buttons.html') },
 	{ title: "Checkboxes & Radios",	html: require('./components/forms/checkbox-radio.html') },
 	{ title: "Inputs",			html: require('./components/forms/input.html') },
-	{ title: "Lists",				html: require('./components/lists.html') },
+	{ title: "Slats",				html: require('./components/slats/slats.html') },
+	{ title: "Tables",				html: require('./components/tables/tables.html') },
+	{ title: "Cards",	html: require('./components/cards/cards.html') },
 	{ title: "Navigation",	html: require('./components/navigation/navigation.html') },
 	{ title: "Textarea",		html: require('./components/forms/textarea.html') }
 ];
