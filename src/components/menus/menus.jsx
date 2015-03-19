10ac7e0d40fbe4e094b3a19825f2c910b8e55122
kavarell <kurt@beanstalkhq.com>
2015-03-19 00:09:19 -0600

    Add dropdown menu

--- src/components/menus/menus.jsx
+++ src/components/menus/menus.jsx
@@ -33,26 +33,22 @@ module.exports = React.createClass({
 						<div className="bss-col-xs-3">
 							<div className="bss-dropdown bss-open">
 								<a href className="bss-link">
-								 Settings
+									<i className="bs-icon bs-icon-Cog"></i>
 								</a>
 								<ul className="bss-dropdown-menu" role="menu">
-									<li><a href>My Profile</a></li>
-									<li><a href>Team Members</a></li>
-									<li><a href>Company Profile</a></li>
+									<li><a href><i className="bs-icon bs-icon-AddPerson bss-dropdown-menu__icon"></i>Share</a></li>
+									<li><a href><i className="bs-icon bs-icon-Download bss-dropdown-menu__icon"></i>Download</a></li>
+									<li><a href><i className="bs-icon bs-icon-Notes bss-dropdown-menu__icon"></i>Rename</a></li>
+									<li><a href><i className="bs-icon bs-icon-Trash bss-dropdown-menu__icon"></i>Delete</a></li>
 
-									<li className="bss-divider"></li>
-									<li><a href="https://beanstalk.reamaze.com" target="_blank">Help</a>
-									</li>
-									<li className="bss-divider"></li>
-									<li><a href="/signout">Sign out</a>
-									</li>
 								</ul>
 							</div>
 						</div>
+
 						<div className="bss-col-xs-3">
-							<div className="bss-dropdown bss-open">
+							<div className="bss-dropdown">
 								<a href className="bss-link">
-								 Settings
+								 Settings <i className="bss-caret-down bss-dropdown__icon"></i>
 								</a>
 								<ul className="bss-dropdown-menu" role="menu">
 									<li><a href>My Profile</a></li>
@@ -71,7 +67,7 @@ module.exports = React.createClass({
 						<div className="bss-col-xs-3">
 							<div className="bss-dropdown bss-pull-right bss-open">
 								<a href className="bss-link">
-								 Settings
+									Settings <i className="bss-caret-down bss-dropdown__icon"></i>
 								</a>
 								<ul className="bss-dropdown-menu bss-pull-right" role="menu">
 									<li><a href>My Profile</a></li>
