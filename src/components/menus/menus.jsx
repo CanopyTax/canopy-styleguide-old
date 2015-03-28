503bb5844b1afa3e1d789d9c16416d926f558172
kavarell <kurt@beanstalkhq.com>
2015-03-28 17:13:38 -0600

    Add menu example

--- src/components/menus/menus.jsx
+++ src/components/menus/menus.jsx
@@ -5,10 +5,33 @@ module.exports = React.createClass({
 	render: function() {
 		return (
 		<div>
-			<div className="bss-fixed-focus bss-card">
+			<div className="bss-fixed-focus bss-card ">
 				<div className="bss-card__header bss-subheader">
 					Menus
 				</div>
+			</div>
+				<Highlight className="html ">
+				{
+`<div className="bss-dropdown bss-open">
+  <a href className="bss-btn +primary dropdown-toggle">
+   New <i className="bss-icon-sm-caret-down bss-dropdown__icon +white"></i>
+  </a>
+  <ul className="bss-dropdown-menu" role="menu">
+    <li><a href>My Profile</a></li>
+    <li><a href>Team Members</a></li>
+    <li><a href>Company Profile</a></li>
+
+    <li className="bss-divider"></li>
+    <li><a href="https://beanstalk.reamaze.com" target="_blank">Help</a>
+    </li>
+    <li className="bss-divider"></li>
+    <li><a href="/signout">Sign out</a>
+    </li>
+  </ul>
+</div>`
+				}
+				</Highlight>
+			<div className="bss-fixed-focus bss-card">
 				<div className="bss-card__body">
 					<div className="bss-row">
 						<div className="bss-col-xs-3">
@@ -86,7 +109,6 @@ module.exports = React.createClass({
 					</div>
 				</div>
 			</div>
-
 		</div>
 		)
 	}
