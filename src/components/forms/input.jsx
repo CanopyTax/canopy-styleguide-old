8fbaa8018217f76dd5cc9b915cc2c98f8e87eb8a
kavarell <kurt@beanstalkhq.com>
2015-03-27 19:51:00 -0600

    Update error handling

--- src/components/forms/input.jsx
+++ src/components/forms/input.jsx
@@ -18,16 +18,18 @@ module.exports = React.createClass({
 									<label for="exampleInputEmail1">Full Name</label>
 									<input type="text" className="bss-form-control" placeholder="Enter name"/>
 								</div>
-								<div className="bss-form-group">
+								<div className="bss-form-group bss-has-feedback">
 									<label for="exampleInputEmail1">Email address</label>
 									<input type="text" className="bss-form-control" placeholder="Enter email"/>
+									<span className="bss-icon-client bss-form-control-feedback" aria-hidden="true"></span>
+										<span className="bss-error-block">This error message is hidden unless bss-has-error is present</span>
 									<span className="bss-help-block">Example help block</span>
 								</div>
-								<div className="bss-form-group bss-has-error bss-has-feedback">
+								<div className="bss-form-group  bss-has-error">
 									<label for="exampleInputPassword1">Phone Number</label>
 									<input type="text" className="bss-form-control" value="234-343-3434"/>
 									<span className="bss-icon-error bss-form-control-feedback" aria-hidden="true"></span>
-									<span className="bss-help-block">This is an error!</span>
+									<span className="bss-error-block">This is an error!</span>
 								</div>
 								<div className="bss-form-group">
 									<label for="exampleInputPassword1">Disabled</label>
@@ -43,15 +45,21 @@ module.exports = React.createClass({
 				{
 `<form>
   <div class="bss-form-group">
-     <label for="exampleInputEmail1">Email address</label>
-     <input type="text" class="bss-form-control" placeholder="Enter email"/>
-     <span class="bss-help-block">Example help block</span>
+    <label for="exampleInputEmail1">Full Name</label>
+    <input type="text" class="bss-form-control" placeholder="Enter name"/>
   </div>
-  <div class="bss-form-group bss-has-error bss-has-feedback">
+  <div class="bss-form-group bss-has-feedback">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="text" class="bss-form-control" placeholder="Enter email"/>
+    <span class="bss-icon-client bss-form-control-feedback" aria-hidden="true"></span>
+      <span class="bss-error-block">This error message is hidden unless bss-has-error is present</span>
+    <span class="bss-help-block">Example help block</span>
+  </div>
+  <div class="bss-form-group  bss-has-error">
     <label for="exampleInputPassword1">Phone Number</label>
     <input type="text" class="bss-form-control" value="234-343-3434"/>
     <span class="bss-icon-error bss-form-control-feedback" aria-hidden="true"></span>
-    <span class="bss-help-block">This is an error!</span>
+    <span class="bss-error-block">This is an error!</span>
   </div>
   <div class="bss-form-group">
     <label for="exampleInputPassword1">Disabled</label>
@@ -73,19 +81,21 @@ module.exports = React.createClass({
 										<input type="text" className="bss-form-control" placeholder="Enter name"/>
 									</div>
 								</div>
-								<div className="bss-form-group">
+								<div className="bss-form-group bss-has-feedback bss-has-error">
 									<label for="exampleInputEmail1" className="bss-col-xs-2">Email address</label>
 									<div className="bss-col-xs-3">
 										<input type="text" className="bss-form-control" placeholder="Enter email"/>
+										<span className="bss-icon-client bss-form-control-feedback" aria-hidden="true"></span>
+										<span className="bss-error-block">This error message is hidden unless bss-has-error is present</span>
 										<span className="bss-help-block">Example help block</span>
 									</div>
 								</div>
-								<div className="bss-form-group bss-has-error bss-has-feedback">
+								<div className="bss-form-group bss-has-error">
 									<label for="exampleInputPassword1" className="bss-col-xs-2">Phone Number</label>
 									<div className="bss-col-xs-6">
 										<input type="text" className="bss-form-control" value="234-343-3434"/>
 										<span className="bss-icon-error bss-form-control-feedback" aria-hidden="true"></span>
-										<span className="bss-help-block">This is an error!</span>
+										<span className="bss-error-block">This is an error!</span>
 									</div>
 								</div>
 								<div className="bss-form-group">
@@ -104,10 +114,32 @@ module.exports = React.createClass({
 				{
 `<form class="bss-form-horizontal">
   <div class="bss-form-group">
+    <label for="exampleInputEmail1" class="bss-col-xs-2">Full Name</label>
+    <div class="bss-col-xs-3">
+      <input type="text" class="bss-form-control" placeholder="Enter name"/>
+    </div>
+  </div>
+  <div class="bss-form-group bss-has-feedback bss-has-error">
     <label for="exampleInputEmail1" class="bss-col-xs-2">Email address</label>
     <div class="bss-col-xs-3">
-      <input type="text" class="bss-form-control" placeholder="Enter email"/>
+      <input type="text" className="bss-form-control" placeholder="Enter email"/>
+      <span class="bss-icon-client bss-form-control-feedback" aria-hidden="true"></span>
+      <span class="bss-error-block">This error message is hidden unless bss-has-error is present</span>
       <span class="bss-help-block">Example help block</span>
+    </div>
+  </div>
+  <div class="bss-form-group bss-has-error">
+    <label for="exampleInputPassword1" class="bss-col-xs-2">Phone Number</label>
+    <div class="bss-col-xs-6">
+      <input type="text" class="bss-form-control" value="234-343-3434"/>
+      <span class="bss-icon-error bss-form-control-feedback" aria-hidden="true"></span>
+      <span class="bss-error-block">This is an error!</span>
+    </div>
+  </div>
+  <div class="bss-form-group">
+    <label for="exampleInputPassword1" class="bss-col-xs-2">Disabled</label>
+    <div class="bss-col-xs-10">
+      <input type="text" class="bss-form-control"value="234-343-3434" disabled="true"/>
     </div>
   </div>
 </form>`
@@ -120,11 +152,12 @@ module.exports = React.createClass({
 					<div className="bss-row bss-padding-bottom-16">
 						<div className="bss-col-xs-12">
 							<form className="bss-form-inline">
-								<div className="bss-form-group">
+								<div className="bss-form-group bss-has-feedback">
 									<label for="exampleInputName2">Email</label>
 									<input type="text" className="bss-form-control" placeholder="Enter email"/>
+									<span className="bss-icon-client bss-form-control-feedback" aria-hidden="true"></span>
 								</div>
-								<div className="bss-form-group bss-has-error bss-has-feedback">
+								<div className="bss-form-group bss-has-error">
 									<label for="exampleInputName2">Phone</label>
 									<input type="text" className="bss-form-control" value="234-343-3434"/>
 									<span className="bss-icon-error bss-form-control-feedback" aria-hidden="true"></span>
@@ -140,11 +173,12 @@ module.exports = React.createClass({
 			<Highlight className="html">
 {
 `<form class="bss-form-inline">
-  <div class="bss-form-group">
+  <div class="bss-form-group bss-has-feedback">
     <label for="exampleInputName2">Email</label>
     <input type="text" class="bss-form-control" placeholder="Enter email"/>
+    <span class="bss-icon-client bss-form-control-feedback" aria-hidden="true"></span>
   </div>
-  <div class="bss-form-group bss-has-error bss-has-feedback">
+  <div class="bss-form-group bss-has-error">
     <label for="exampleInputName2">Phone</label>
     <input type="text" class="bss-form-control" value="234-343-3434"/>
     <span class="bss-icon-error bss-form-control-feedback" aria-hidden="true"></span>
