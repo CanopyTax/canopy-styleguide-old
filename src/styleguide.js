99f47be496adf400433dc1c45d9e6a9465078768
Joel Denning <joeldenning@gmail.com>
2016-12-01 13:03:21 -0700

    Switching to asap webfont

--- src/styleguide.js
+++ src/styleguide.js
@@ -1,1 +1,8 @@
 require('./components.less');
+
+var el = document.createElement('link'); 
+el.setAttribute("rel", "stylesheet");
+el.setAttribute("href", "https://fonts.googleapis.com/css?family=Asap:400italic,600italic,300,400,600");
+el.setAttribute("type","text/css");
+var head = document.getElementsByTagName('head')[0];
+head.appendChild(el);
