fceb432c8e055e12708bbe23a8bea1f3e38a44b7
Joel Denning <joeldenning@gmail.com>
2016-12-02 14:29:10 -0700

    Loading font-weight 700 instead of 600

--- src/styleguide.js
+++ src/styleguide.js
@@ -2,7 +2,11 @@ require('./components.less');
 
 var el = document.createElement('link'); 
 el.setAttribute("rel", "stylesheet");
-el.setAttribute("href", "https://fonts.googleapis.com/css?family=Asap:400italic,600italic,300,400,600");
+/* Asap only has font-weights of 400 and 700. All of our references to font-weight: 600
+ * will be rendered as font-weight 700 because of the CSS specification. See
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight "Fallback" section
+ */
+el.setAttribute("href", "https://fonts.googleapis.com/css?family=Asap:400,400i,700,700i");
 el.setAttribute("type","text/css");
 var head = document.getElementsByTagName('head')[0];
 head.appendChild(el);
