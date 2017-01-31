1fc0ee1e0b41848c0407b81d8c8efec1fc20a8a4
Joel Denning <joeldenning@gmail.com>
2017-01-31 16:17:20 -0700

    Added cross-browser testing for custom elements.

---
+++ src/custom-elements/custom-elements.spec.js
@@ -0,0 +1,7 @@
+import 'babel-polyfill';
+import './force-polyfill.js';
+import '@joeldenning/custom-elements';
+import './definitions.js';
+
+// https://github.com/webcomponents/custom-elements#differences-from-spec
+customElements.enableFlush = true;
