3af70c03d9683ecb878e78dfabdc991d6233dd73
Daniel Kesler <danielckesler@gmail.com>
2017-04-11 14:01:19 -0600

    Exporting calendar (#73)

--- src/custom-elements/definitions.js
+++ src/custom-elements/definitions.js
@@ -1,5 +1,6 @@
 import './cps-button/cps-button.js';
 import './cps-tooltip/cps-tooltip.js';
 import './cps-datepicker/cps-datepicker.js';
+import './cps-datepicker/cps-calendar.component.js';
 import './cps-colorpicker/cps-colorpicker.js';
 import './cps-icon/cps-icon.js';
