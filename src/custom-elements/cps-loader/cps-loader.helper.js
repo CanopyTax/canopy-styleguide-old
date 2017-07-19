1d5c1277a44048793acac0becf5a09985fc524f0
Bret Little <bret.little@gmail.com>
2017-07-19 15:18:17 -0600

    Change the width of the loader component to make it easier to center

--- src/custom-elements/cps-loader/cps-loader.helper.js
+++ src/custom-elements/cps-loader/cps-loader.helper.js
@@ -4,28 +4,30 @@ export function determineDotWidth(props){
 		let size = parseInt(props.dotSize);
 		if(isNaN(size)){
 			size = props.page ? 42 : 8;
-		} 
+		}
 		return size;
 }
 
 export function makeDots(props){
     let dots = [];
     for(let i= 0; i < 3; i++){
-        dots.push(makeDot(props));
+        dots.push(makeDot(props, i === 2));
     }
     return dots;
 }
 
-export function makeDot(props){
+export function makeDot(props, last){
     const size = determineDotWidth(props);
+		const styles = {
+			backgroundColor: props.color || '#e0e0e0',
+			width: size,
+			height: size,
+			marginLeft: size,
+		};
+
+		if (last) styles.marginRight = size;
+
     return (
-        <span style={
-            {
-                backgroundColor: props.color || '#e0e0e0',
-                width: size,
-                height: size,
-                marginLeft: size,
-            }
-        } />
+        <span style={styles} />
     )
 }
