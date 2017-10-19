2831bd089de8f506b066bcde7db19545c6fa821e
Kent McLean <kent.mclean@canopytax.com>
2017-10-19 10:20:02 -0600

    slide component skeleton

---
+++ src/custom-elements/cps-slide-dialog/Readme.md
@@ -0,0 +1,9 @@
+# cps-slide-dialog
+Wraps the inner content in a dialog that slides out from the upper-right portion of the screen
+
+## HTML Usage
+```html
+<cps-slide-dialog hide-x>
+	Whatever content|app|component you want to be wrapped within the dialog
+</cps-slide-dialog>
+```
---
+++ src/custom-elements/cps-slide-dialog/cps-slide-dialog.component.js
@@ -0,0 +1,89 @@
+import { Component, h } from 'preact';
+import { NavContent } from 'primary-navbar!sofe';
+import { customElementToReact } from '../react-interop.js';
+import { preactToCustomElement } from '../preact-to-custom-element.js';
+import styles from './cps-slide-dialog.styles.css';
+
+class CpsSlideDialog extends Component {
+	componentDidMount() {
+		this.dialog.addEventListener('animationend', this.OnShowComplete, false);
+	}
+
+	state = {
+		beenDismissed: false,
+	};
+
+	render() {
+		return (
+			<NavContent
+				hasTopnavSecondary={false}
+				contactMenuPossible={true}
+				style={{ marginLeft: 0, width: '100%', padding: 0 }}>
+				<div
+					ref={el => {
+						this.dialog = el;
+					}}
+					className={`${styles.slideDialog} ${this.state.beenDismissed ? styles.out : styles.in}`}>
+					<div className={`cps-card__height-3 cps-padding-24 ${styles.card}`}>
+						<TheX />
+						<div>
+							{this.props.children({
+								show,
+								hide,
+								destroy,
+								onShowReady,
+								onShowComplete,
+								onHideComplete,
+							})}
+						</div>
+					</div>
+				</div>
+			</NavContent>
+		);
+	}
+
+	componentWillUnmount() {
+		this.dialog.removeEventListener('animationend', this.onShowComplete, false);
+	}
+
+	onShowReady() {}
+
+	onShowComplete() {}
+
+	onHideComplete() {}
+
+	show = () => {
+		this.setState((prevProps, props) => ({
+			show: true,
+		}));
+	};
+
+	hide = () => {
+		this.setState((prevProps, props) => ({
+			show: false,
+		}));
+	};
+
+	destroy = () => {};
+
+	TheX = () => {
+		return (
+			this.props.hideX && (
+				<div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+					<a className="cps-link">
+						<span className="cps-icon cps-icon-close" />
+					</a>
+				</div>
+			)
+		);
+	};
+}
+
+const customElement = preactToCustomElement(CpsSlideDialog, {
+	parentClass: HTMLElement,
+	properties: ['hideX'],
+});
+customElements.define('cps-slide-dialog', customElement);
+export const CprSlideDialog = customElementToReact({
+	name: 'cps-slide-dialog',
+});
