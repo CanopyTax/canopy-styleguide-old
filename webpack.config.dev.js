28f93337f4ceb23e6ec25bc82e9aec8d7c8791f4
Kent McLean <kent.mclean@canopytax.com>
2017-11-21 13:19:23 -0700

    remove webpack-dashboard. Adjust input validation warning color

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,6 @@
 /* eslint-env node */
-const DashboardPlugin = require('webpack-dashboard/plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const config = require('./webpack.config.sofe.js');
-
-config.plugins.push(new DashboardPlugin());
 
 // config.plugins.push(
 // 	new BundleAnalyzerPlugin({
