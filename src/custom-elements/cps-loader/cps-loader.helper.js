09c96c19a22a59c3c68f041c5dabc324b50bb4d9
Justin McMurdie <mcmurdiej@gmail.com>
2017-12-01 13:10:20 -0700

    Here comes the December Easter Egg

--- src/custom-elements/cps-loader/cps-loader.helper.js
+++ src/custom-elements/cps-loader/cps-loader.helper.js
@@ -1,7 +1,14 @@
 import { h } from 'preact';
+import { merge } from 'lodash'
+
+const eventLoaderMap = getEventLoaderMap()
+const occasion = getOccasion()
+const baseSpecialOccasionStyles = getBaseSpecialOccasionStyles(occasion)
+const isSpecialOccasion = !!baseSpecialOccasionStyles
 
 export function makeDots(props) {
 	let dots = [];
+  props.specialOccasionStyles = baseSpecialOccasionStyles
 	for (let i = 0; i < 3; i++) {
 		dots.push(makeDot(props, i === 2));
 	}
@@ -17,9 +24,11 @@ export function makeDot(props, last) {
 		marginLeft: size,
 	};
 
+  const mergedStyles = merge({}, styles, props.specialOccasionStyles)
+
 	if (last) styles.marginRight = size;
 
-	return <span style={styles} />;
+	return <span style={mergedStyles} />;
 }
 
 export function determineDotWidth(props) {
@@ -28,4 +37,31 @@ export function determineDotWidth(props) {
 		size = props.page ? 42 : 8;
 	}
 	return size;
+}
+
+export function getOccasion() {
+  if (window.spalpatineInitialTime) {
+    const date = new Date(window.spalpatineInitialTime)
+    if (date.getMonth() === 11) {
+      return 'december'
+    } else {
+      return undefined
+    }
+  }
+}
+
+export function getBaseSpecialOccasionStyles(occasion) {
+  return eventLoaderMap[occasion]
+}
+
+function getEventLoaderMap() {
+  return {
+    december: {
+      backgroundImage: "url('https://cdn.canopytax.com/images/light_lit.svg')",
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundColor: 'transparent'
+    },
+  }
 }
