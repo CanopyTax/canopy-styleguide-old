38f74d071747c0c8ee3aa9901c17b0eed333a8b8
Justin McMurdie <mcmurdiej@gmail.com>
2017-12-01 14:24:40 -0700

    Pr/special occasion easter egg (#113)
    * Here comes the December Easter Egg
    
    * Adding test for getOccasion
    
    * spaces not tabs
    
    * Refactor to not use lodash merge.
    
    * Making returns consistent
    
    * Adding horrific ability to turn off easter eggs
    
    * Removing excess functions

--- src/custom-elements/cps-loader/cps-loader.helper.js
+++ src/custom-elements/cps-loader/cps-loader.helper.js
@@ -1,31 +1,70 @@
 import { h } from 'preact';
 
+const eventLoaderMap = getEventLoaderMap()
+const baseSpecialOccasionStyles = getBaseSpecialOccasionStyles(getOccasion())
+
 export function makeDots(props) {
-	let dots = [];
-	for (let i = 0; i < 3; i++) {
-		dots.push(makeDot(props, i === 2));
-	}
-	return dots;
+  let dots = [];
+  for (let i = 0; i < 3; i++) {
+    dots.push(makeDot(props, i === 2));
+  }
+  return dots;
 }
 
 export function makeDot(props, last) {
-	const size = determineDotWidth(props);
-	const styles = {
-		backgroundColor: props.color || '#e0e0e0',
-		width: size,
-		height: size,
-		marginLeft: size,
-	};
+  const size = determineDotWidth(props);
+  const styles = {
+    backgroundColor: props.color || '#e0e0e0',
+    width: size,
+    height: size,
+    marginLeft: size,
+    ...baseSpecialOccasionStyles
+  };
 
-	if (last) styles.marginRight = size;
+  if (last) styles.marginRight = size;
 
-	return <span style={styles} />;
+  return <span style={styles} />;
 }
 
 export function determineDotWidth(props) {
-	let size = parseInt(props.dotSize);
-	if (isNaN(size)) {
-		size = props.page ? 42 : 8;
-	}
-	return size;
+  let size = parseInt(props.dotSize);
+  if (isNaN(size)) {
+    size = props.page ? 42 : 8;
+  }
+  return size;
+}
+
+export function getOccasion() {
+  if (window.spalpatineInitialTime) {
+    const date = new Date(window.spalpatineInitialTime)
+    if (date.getMonth() === 11) {
+      return 'december'
+    } else {
+      return ''
+    }
+  }
+}
+
+export function getBaseSpecialOccasionStyles(occasion) {
+  const noEasterEggs = localStorage.getItem('cp:no-easter-eggs') === 'true'
+  if (noEasterEggs) {
+    return {}
+  } else {
+    return eventLoaderMap[occasion] || {}
+  }
+}
+
+function getEventLoaderMap() {
+  const imageCommon = {
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundColor: 'transparent'
+  }
+  return {
+    december: {
+      backgroundImage: "url('https://cdn.canopytax.com/images/light_lit.svg')",
+      ...imageCommon
+    },
+  }
 }
--- src/custom-elements/cps-loader/cps-loader.test.js
+++ src/custom-elements/cps-loader/cps-loader.test.js
@@ -1,23 +1,35 @@
-import { makeDots, makeDot, determineDotWidth } from './cps-loader.helper.js';
+import { makeDots, makeDot, determineDotWidth, getOccasion } from './cps-loader.helper.js';
 
 describe(`cps-loader.js`, () => {
-	it(`only creates 3 dots`, () => {
-		expect(makeDots({}).length).toBe(3);
-	});
 
-	it(`defaults to 8 pixels`, () => {
-		expect(determineDotWidth({})).toBe(8);
-	});
+  describe(`creating dots`, () => {
+    it(`only creates 3 dots`, () => {
+      expect(makeDots({}).length).toBe(3);
+    });
 
-	it(`defaults to 42 pixels when the page attribute is set`, () => {
-		expect(determineDotWidth({ page: true })).toBe(42);
-	});
+    it(`defaults to 8 pixels`, () => {
+      expect(determineDotWidth({})).toBe(8);
+    });
 
-	it(`respects the dotSize attribute`, () => {
-		expect(determineDotWidth({ dotSize: 24 })).toBe(24);
-	});
+    it(`defaults to 42 pixels when the page attribute is set`, () => {
+      expect(determineDotWidth({ page: true })).toBe(42);
+    });
 
-	it(`gives priority to the dotSize attribute over the page attribute`, () => {
-		expect(determineDotWidth({ page: true, dotSize: 24 })).toBe(24);
-	});
+    it(`respects the dotSize attribute`, () => {
+      expect(determineDotWidth({ dotSize: 24 })).toBe(24);
+    });
+
+    it(`gives priority to the dotSize attribute over the page attribute`, () => {
+      expect(determineDotWidth({ page: true, dotSize: 24 })).toBe(24);
+    });
+
+  })
+
+  describe(`getOccasion`, () => {
+    it(`should return december`, () => {
+      window.spalpatineInitialTime = 1512158983477
+      expect(getOccasion()).toBe('december')
+      window.spalpatineInitialTime = undefined
+    })
+  })
 });
