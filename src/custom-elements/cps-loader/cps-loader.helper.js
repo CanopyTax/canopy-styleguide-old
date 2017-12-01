79aa0ca633b7fc671838c3eca7763f1ce74865f5
Justin McMurdie <mcmurdiej@gmail.com>
2017-12-01 13:17:44 -0700

    spaces not tabs

--- src/custom-elements/cps-loader/cps-loader.helper.js
+++ src/custom-elements/cps-loader/cps-loader.helper.js
@@ -7,36 +7,36 @@ const baseSpecialOccasionStyles = getBaseSpecialOccasionStyles(occasion)
 const isSpecialOccasion = !!baseSpecialOccasionStyles
 
 export function makeDots(props) {
-	let dots = [];
+  let dots = [];
   props.specialOccasionStyles = baseSpecialOccasionStyles
-	for (let i = 0; i < 3; i++) {
-		dots.push(makeDot(props, i === 2));
-	}
-	return dots;
+  for (let i = 0; i < 3; i++) {
+    dots.push(makeDot(props, i === 2));
+  }
+  return dots;
 }
 
 export function makeDot(props, last) {
-	const size = determineDotWidth(props);
-	const styles = {
-		backgroundColor: props.color || '#e0e0e0',
-		width: size,
-		height: size,
-		marginLeft: size,
-	};
+  const size = determineDotWidth(props);
+  const styles = {
+    backgroundColor: props.color || '#e0e0e0',
+    width: size,
+    height: size,
+    marginLeft: size,
+  };
 
   const mergedStyles = merge({}, styles, props.specialOccasionStyles)
 
-	if (last) styles.marginRight = size;
+  if (last) styles.marginRight = size;
 
-	return <span style={mergedStyles} />;
+  return <span style={mergedStyles} />;
 }
 
 export function determineDotWidth(props) {
-	let size = parseInt(props.dotSize);
-	if (isNaN(size)) {
-		size = props.page ? 42 : 8;
-	}
-	return size;
+  let size = parseInt(props.dotSize);
+  if (isNaN(size)) {
+    size = props.page ? 42 : 8;
+  }
+  return size;
 }
 
 export function getOccasion() {
--- src/custom-elements/cps-loader/cps-loader.js
+++ src/custom-elements/cps-loader/cps-loader.js
@@ -5,18 +5,18 @@ import styles from './cps-loader.styles.css';
 import { makeDots } from './cps-loader.helper.js';
 
 class CpsLoader extends Component {
-	render() {
-		return (
-			<div className={`${styles.loader} ${this.props.customElement.page ? styles.page : ''}`}>
-				{makeDots(this.props.customElement)}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={`${styles.loader} ${this.props.customElement.page ? styles.page : ''}`}>
+        {makeDots(this.props.customElement)}
+      </div>
+    );
+  }
 }
 
 const customElement = preactToCustomElement(CpsLoader, {
-	parentClass: HTMLElement,
-	properties: ['page', 'dotSize', 'color'],
+  parentClass: HTMLElement,
+  properties: ['page', 'dotSize', 'color'],
 });
 customElements.define('cps-loader', customElement);
 export const CprLoader = customElementToReact({ name: 'cps-loader' });
